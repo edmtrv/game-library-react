5007017560545d28fba433c597509fe02138eca6
Emil Dimitrov <edimitrov1@gmail.com>
2020-01-27 23:03:43 +0200

    Games list strucutre

---
+++ src/components/GamesList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const GamesList = () => {
+  return (
+    <div>
+      <main class="container">
+        <div class="row">
+          <header class="col">
+            <h2 class="h2">Games</h2>
+          </header>
+        </div>
+        <div class="row">
+          <div class="col">
+            <section class="games"></section>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default GamesList;
