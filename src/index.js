e7f21c974cc3f08a300da13864b78aebeb949572
Emil Dimitrov <edimitrov1@gmail.com>
2020-01-27 22:38:42 +0200

    Move App to a stand-alone component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <div>App</div>
-};
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
