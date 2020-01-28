7e5513d9584aa17004161a31c8e359fbf4f9282d
Emil Dimitrov <edimitrov1@gmail.com>
2020-01-28 23:07:13 +0200

    Make form inputs controlled

--- src/components/GameModal.js
+++ src/components/GameModal.js
@@ -1,71 +1,87 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const GameModal = ({ modal, toggle }) => {
-  return (
-    <div>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <form>
-          <ModalBody>
-            <div className="form-group">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                name="title"
-                id="title"
-                className="form-control"
-                placeholder="Enter Title"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="genre">Genre</label>
-              <input
-                type="text"
-                name="genre"
-                id="genre"
-                className="form-control"
-                placeholder="Enter Genre"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="price">Price</label>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                className="form-control"
-                placeholder="In €"
-                step="0.01"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <textarea
-                name="description"
-                id="sescription"
-                rows="3"
-                className="form-control"
-                placeholder="Enter Description"
-                required
-              ></textarea>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={toggle}>
-              Add Game
-            </Button>{' '}
-            <Button color="secondary" onClick={toggle}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </form>
-      </Modal>
-    </div>
-  );
-};
+class GameModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.sttate = { title: '', genre: '', price: null, description: '' };
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal isOpen={this.props.modal} toggle={this.props.toggle}>
+          <ModalHeader toggle={this.props.toggle}>Modal title</ModalHeader>
+          <form>
+            <ModalBody>
+              <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input
+                  onChange={e => this.setState({ title: e.target.value })}
+                  value={this.state.title}
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="form-control"
+                  placeholder="Enter Title"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="genre">Genre</label>
+                <input
+                  onChange={e => this.setState({ genre: e.target.value })}
+                  value={this.state.genre}
+                  type="text"
+                  name="genre"
+                  id="genre"
+                  className="form-control"
+                  placeholder="Enter Genre"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="price">Price</label>
+                <input
+                  onChange={e => this.setState({ price: e.target.value })}
+                  value={this.state.price}
+                  type="number"
+                  name="price"
+                  id="price"
+                  className="form-control"
+                  placeholder="In €"
+                  step="0.01"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  onChange={e => this.setState({ description: e.target.value })}
+                  value={this.state.description}
+                  name="description"
+                  id="sescription"
+                  rows="3"
+                  className="form-control"
+                  placeholder="Enter Description"
+                  required
+                ></textarea>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.propstoggle}>
+                Add Game
+              </Button>{' '}
+              <Button color="secondary" onClick={this.props.toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </form>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 export default GameModal;
