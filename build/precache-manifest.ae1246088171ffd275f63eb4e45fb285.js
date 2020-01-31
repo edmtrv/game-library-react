5f628efc6228ca7ba8b544305bb456ecb57ad09c
Emil Dimitrov <edimitrov1@gmail.com>
2020-01-31 23:58:26 +0200

    Remove build folder from gitignore

---
+++ build/precache-manifest.ae1246088171ffd275f63eb4e45fb285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009cf577bd764d76351ce0e135294f69",
+    "url": "/game-library-react/index.html"
+  },
+  {
+    "revision": "cd02cec5ed20f05e8392",
+    "url": "/game-library-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cd02cec5ed20f05e8392",
+    "url": "/game-library-react/static/js/2.81ad58b1.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/game-library-react/static/js/2.81ad58b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fc50f9a68ac7be125b9",
+    "url": "/game-library-react/static/js/main.8ec1e650.chunk.js"
+  },
+  {
+    "revision": "3db96ae4a30f110b81e9",
+    "url": "/game-library-react/static/js/runtime-main.de913bea.js"
+  }
+]);
